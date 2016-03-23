92f8273198a1bede2b4b760417d4c52343437fd5
Pioneer11X <sravan@mysmartprice.com>
2016-03-24 02:28:12 +0530

    what is wrong here?

--- getMspWebsitePrice.py
+++ getMspWebsitePrice.py
@@ -4,58 +4,28 @@ import keys
 import pymysql
 import pymysql.cursors
 
-
 # first we need to formualte the url
 
 # We need to get the popular subcategories from our DB and for that we need to connect to our db
 
-'''
-
-connection = pymysql.connect(
-		host=keys.mysqlHost,
-		user=keys.mysqlUser,
-		password=keys.mysqlPass,
-		db=keys.mysqlDbName,
-		charset='utf8mb4',
-		cursorclass=pymysql.cursors.DictCursor)
-
-try:
-	with connection.cursor() as cursor:
-		sql = "select * from fashion_lines where store = 'jabong' limit 1"
-		cursor.execute(sql)
-		result = cursor.fetchone()
-		print(result)
-
-finally:
-	connection.close()
-
-'''
-
-
 class StoreQA:
 
-	store = "jabong"
-	
-	'''
-	def __init__(self, store):
-		self.store = store
-		self.result = self.getPopSubCat(store)
-	'''
-	
+    store = "jabong"
 
-	def getPopSubCat(self):
-		connection = pymysql.connect(
-				host=keys.mysqlHost,
-				user=keys.mysqlUser,
-				password=keys.mysqlPass,
-				db=keys.mysqlDbName,
-				charset='utf8mb4',
-				cursorclass=pymysql.cursors.DictCursor)
-		print "here"
+    def getPopSubCat(self):
+
+        connection = pymysql.connect(
+                host=keys.mysqlHost,
+                user=keys.mysqlUser,
+                password=keys.mysqlPass,
+                db=keys.mysqlCrawlDb,
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor)
 
 		try:
 			with connection.cursor() as cursor:
-				sql = "select distinct fel.category, felsubcategory from fashion_entry_url fel, subcategory_crawl_config scc where fel.category = scc.category and fel.store ='" + self.store + "' and scc.crawl_day_if_week = -1"
+				# sql = "select item_id from fashion_lines where store = '" + self.store + "' limit 1"
+				sql = "select distinct fel.category, fel.subcategory from fashion_entry_url fel, subcategory_crawl_config scc where fel.category = scc.category and fel.store ='" + self.store + "' and scc.crawl_day_of_week = -1"
 				cursor.execute(sql)
 				result = cursor.fetchone()
 				print(result)
@@ -66,12 +36,10 @@ class StoreQA:
 			connection.close()
 			print ( result )
 
-
-if __name__ == " ___main__":
-	store = "jabong"
-	# test = StoreQA(store)
-	test = StoreQA()
-	print ( test )
-	test.getPopSubCat()
+		return
 
 
+
+store = "jabong"
+test = StoreQA()
+test.getPopSubCat()
